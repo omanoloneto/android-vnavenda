218187944799ae6d5564c3a295c04c9807408d57
Manoel Afonso <mano.afonso93@gmail.com>
2024-01-10 21:49:32 -0300

    Fixing app build

--- app/src/main/java/br/com/manoloneto/vnavenda/ui/screens/ShoppingListScreen.kt
+++ app/src/main/java/br/com/manoloneto/vnavenda/ui/screens/ShoppingListScreen.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import br.com.manoloneto.vnavenda.viewmodels.ShoppingListViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import br.com.manoloneto.vnavenda.ui.composables.ShoppingItemRow
+import br.com.manoloneto.vnavenda.viewmodels.ShoppingListViewModel
 import java.util.Collections.emptyList
 
 @Composable
-fun ShoppingListScreen(viewModel: ShoppingListViewModel = hiltViewModel()) {
+fun ShoppingListScreen() {
+    val viewModel: ShoppingListViewModel = hiltViewModel()
     val shoppingItems by viewModel.shoppingItems.observeAsState(initial = emptyList())
 
     LazyColumn {
