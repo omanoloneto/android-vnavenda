a12d3e478da3ee01ab4f71d2ffe016a7f905caa6
Manoel Afonso <mano.afonso93@gmail.com>
2024-07-09 17:02:32 -0300

    Creating Floating button

--- app/src/main/java/br/com/manoloneto/vnavenda/ui/composables/ShoppingItemRow.kt
+++ app/src/main/java/br/com/manoloneto/vnavenda/ui/composables/ShoppingItemRow.kt
@@ -1,9 +1,33 @@
 package br.com.manoloneto.vnavenda.ui.composables
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import br.com.manoloneto.vnavenda.data.entities.ShoppingItem
 
 @Composable
 fun ShoppingItemRow(item: ShoppingItem, onCheckedChange: (ShoppingItem) -> Unit) {
-    // Something here
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onCheckedChange(item.copy(isChecked = item.isChecked.not())) }
+            .padding(16.dp)
+    ) {
+        Checkbox(
+            checked = item.isChecked,
+            onCheckedChange = {
+                onCheckedChange(item.copy(isChecked = it))
+            }
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = item.name)
+    }
 }
--- app/src/main/java/br/com/manoloneto/vnavenda/ui/screens/ShoppingListScreen.kt
+++ app/src/main/java/br/com/manoloneto/vnavenda/ui/screens/ShoppingListScreen.kt
@@ -2,9 +2,15 @@ package br.com.manoloneto.vnavenda.ui.screens
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import br.com.manoloneto.vnavenda.data.entities.ShoppingItem
 import br.com.manoloneto.vnavenda.ui.composables.ShoppingItemRow
 import br.com.manoloneto.vnavenda.viewmodels.ShoppingListViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -13,11 +19,24 @@ import org.koin.androidx.compose.koinViewModel
 fun ShoppingListScreen(viewModel: ShoppingListViewModel = koinViewModel()) {
     val shoppingItems by viewModel.shoppingItems.observeAsState(initial = emptyList())
 
-    LazyColumn {
-        items(shoppingItems) { item ->
-            ShoppingItemRow(item, onCheckedChange = { updatedItem ->
-                viewModel.updateItem(updatedItem)
-            })
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                val newItem = ShoppingItem(id = 0, name = "New Item", isChecked = false)
+                viewModel.addItem(newItem)
+            }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add new item")
+            }
+
+        }
+    ) { innerPadding ->
+        LazyColumn(contentPadding = innerPadding) {
+            items(shoppingItems) { item ->
+                ShoppingItemRow(item, onCheckedChange = { updatedItem ->
+                    viewModel.updateItem(updatedItem)
+                })
+            }
         }
     }
+
 }
